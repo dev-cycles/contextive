7f1ec4616c1018ab33213164bf5357fb1d27daec
Chris Simon <chris@devcycles.io>
2021-11-06 06:09:34 +0000

    Tweak to names and paths of test results

--- src/vscode/ubictionary/test/index.js
+++ src/vscode/ubictionary/test/index.js
@@ -13,7 +13,7 @@ function run() {
 	.reporter('mocha-multi-reporters', {
 		reporterEnabled: "spec, mocha-junit-reporter",
 		mochaJunitReporterReporterOptions: {
-			mochaFile: path.resolve(testsRoot, `../TestResults-${process.env.DOTNET_VERSION}.xml`)
+			mochaFile: path.resolve(testsRoot, `../TestResults/TestResults-${process.env.DOTNET_VERSION}.xml`)
 		}
 	});
 
