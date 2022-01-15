9c4639e6c4ab7f3845e0c403a9c50d76ec4df9a4
Chris Simon <chris@devcycles.io>
2022-01-16 05:56:34 +1100

    fix(devcontainer): Resolve adr template default setting by ensuring /home/vscode/.config exists in advance, and running scripts with  to ensure profile env variables are loaded. (#20)

--- .devcontainer/setup_tools.sh
+++ .devcontainer/setup_tools.sh
@@ -2,15 +2,24 @@
 source /usr/local/share/nvm/nvm.sh
 # Install npm globals
 npm install -g yo generator-code vsce
+
 # Install dotnet global tools
-PATH=/home/vscode/.dotnet/tools:$PATH
+echo "export PATH=$PATH:~/.dotnet/tools" >> /home/vscode/.bashrc
+PATH=$PATH:~/.dotnet/tools
+echo 
 dotnet new --install Fable.Template
 dotnet tool install dotnet-suggest --global
 dotnet tool install paket --global
 dotnet tool install fable --global
 dotnet tool install femto --global
-# no stable version of the adr tool available yet available yet \n\
-dotnet tool install adr --global --version 0.1.0-preview.36
-adr templates default set madr
 
-echo "export PATH=$PATH" >> /home/vscode/.bashrc
+# ADR tool
+# no stable version of the adr tool available yet
+dotnet tool install adr --global --version 0.1.0-preview.36 
+
+# Setup default adr template
+# Make sure .config exists in the home folder.  If .config doesn't exist yet,
+# the adr config ends up in $HOME/endjin which doesn't work once we're in the
+# container, because by then .config _does_ exist
+mkdir -p $HOME/.config
+adr templates default set madr
