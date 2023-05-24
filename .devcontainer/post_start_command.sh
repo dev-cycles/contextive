d89fdc9168bd70550b801bc3eb2dbdfe06e404c2
Chris Simon <chris@devcycles.io>
2023-05-24 14:02:01 +0000

    chore: update devcontainer post_start_command to use paket instead of dotnet

--- .devcontainer/post_start_command.sh
+++ .devcontainer/post_start_command.sh
@@ -1,1 +1,3 @@
-find ./ -type f -name "*.fsproj" -print0 | xargs -r0 /bin/bash -c 'for f in "$@"; do echo "$f"; dotnet restore "$f"; done;' ''
+find ./ -type f -name "*.sln" -print0 | xargs -r0 /bin/bash -c 'for f in "$@"; do echo "$f"; cd "$(dirname $f)"; paket restore; done;' ''
+cd src/vscode/contextive
+npm install
