9fb80cd9fb9e5494665d221810c10cb7b843e504
Chris Simon <chris@devcycles.io>
2023-05-31 07:47:02 +0000

    chore(cloud): add initial baseline structure of cloud services including cdk infra definition and lambda-hosted api

--- .devcontainer/setup_tools.sh
+++ .devcontainer/setup_tools.sh
@@ -30,3 +30,6 @@ echo "export DBUS_SESSION_BUS_ADDRESS=unix:path=$XDG_RUNTIME_DIR/bus" >> ~/.bash
 sudo mkdir -p $XDG_RUNTIME_DIR
 sudo chmod 700 $XDG_RUNTIME_DIR
 sudo chown $(id -un):$(id -gn) $XDG_RUNTIME_DIR
+
+# Add lambda templates
+dotnet new -i Amazon.Lambda.Templates
