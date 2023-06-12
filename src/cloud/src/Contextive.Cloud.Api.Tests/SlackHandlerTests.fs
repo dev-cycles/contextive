e2cf70bedb47f9fc6bc62cc7c5656aee0918b806
Chris Simon <chris@devcycles.io>
2023-06-12 17:09:30 +0000

    Able to receive slack events and log message payloads. Tested in deployed environment

--- src/cloud/src/Contextive.Cloud.Api.Tests/SlackHandlerTests.fs
+++ src/cloud/src/Contextive.Cloud.Api.Tests/SlackHandlerTests.fs
@@ -10,7 +10,28 @@ type SlackChallenge = JsonProvider<"""{
     "token": "token",
     "challenge": "challenge",
     "type": "url_verification"
-}""", RootName="Challenge", PreferDictionaries=true>
+}""", RootName="Challenge">
+
+type SlackMessage = JsonProvider<"""{
+    "token": "one-long-verification-token",
+    "team_id": "T123ABC456",
+    "api_app_id": "A123ABC456",
+    "event": {
+        "type": "message",
+        "channel": "C123ABC456",
+        "user": "U123ABC456",
+        "text": "Live long and prospect.",
+        "ts": "1355517523.000005",
+        "event_ts": "1355517523.000005",
+        "channel_type": "channel"
+    },
+    "type": "event_callback",
+    "authed_teams": [
+        "T123ABC456"
+    ],
+    "event_id": "Ev123ABC456",
+    "event_time": 1355517523
+}""", RootName="Message">
 
 
 [<Tests>]
@@ -26,6 +47,13 @@ let slackHandlerTests =
                     ``type``="url_verification"
                 )
             let! response = lambdaRequest lambdaFunction "POST" "/slack" (Some <| challenge.JsonValue.ToString())
-            test <@ (response.StatusCode,challengeValue) = (200, response.Body) @>
+            test <@ (response.StatusCode,response.Body) = (200, challengeValue) @>
+        }
+
+        testAsync "Can receive new message and new emoji notifications" {
+            let lambdaFunction = LambdaEntryPoint()
+            let msg = SlackMessage.GetSample()
+            let! response = lambdaRequest lambdaFunction "POST" "/slack" (Some <| msg.JsonValue.ToString())
+            test <@ (response.StatusCode, response.Body) = (200, "") @>
         }
     ]
--- src/cloud/src/Contextive.Cloud/Program.fs
+++ src/cloud/src/Contextive.Cloud/Program.fs
@@ -5,7 +5,6 @@ open Amazon.CDK
 [<EntryPoint>]
 let main _ =
     let app = App(null)
-
     let contextiveCloud = ContextiveCloudStack(app, "ContextiveCloud", StackProps())
     Tags.Of(contextiveCloud).Add("app-name", "ContextiveCloud") |> ignore
     app.Synth() |> ignore
