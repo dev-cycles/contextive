ece4aad91250189e3dd3d2f8e312918549d799e5
Chris Simon <chris@devcycles.io>
2024-05-22 09:12:49 +0200

    chore: start to setup local environment for using Rider under nix-shell for local dev

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    dotnet-sdk_7
+  ];
+}
