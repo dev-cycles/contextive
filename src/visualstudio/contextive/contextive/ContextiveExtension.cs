2040e41e43c6f0590aa16a5a8e81e19fcec72c89
Florian Hopfner <florian.hopfner@inway.de>
2024-07-14 17:46:48 +0200

    feat(visual-studio): Add Visual Studio integration
    #28

---
+++ src/visualstudio/contextive/contextive/ContextiveExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace contextive
+{
+    [VisualStudioContribution]
+    internal class ContextiveExtension : Extension
+    {
+        /// <inheritdoc/>
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            Metadata = new(
+                    id: "contextive.f050f6e5-c51b-4c33-9a59-ba9945a00259",
+                    version: this.ExtensionAssemblyVersion,
+                    publisherName: "Dev Cycles",
+                    displayName: "Contextive",
+                    description: "Supports developers where a complex domain or project specific language is in use by surfacing definitions everywhere specific words are used - code, comments, config or documentation."),
+        };
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+        }
+    }
+}
