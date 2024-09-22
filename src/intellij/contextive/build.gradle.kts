99ac5b70bbb6be22f3adb7ba8455b63ba3d5219c
Chris Simon <chris@chrissimon.au>
2024-09-22 10:53:54 +1000

    ci(intellij): Use a non-EAP version for building and testing

--- src/intellij/contextive/build.gradle.kts
+++ src/intellij/contextive/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("243-EAP-SNAPSHOT")
+  version.set("2024.2.2")
   type.set("IU") // Target IDE Platform
 
   plugins.set(listOf(/* Plugin Dependencies */))
