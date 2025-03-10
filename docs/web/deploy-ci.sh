aa5f6b9921bba70b7c8f3a22b93a696b34facfef
Chris Simon <chris@chrissimon.au>
2025-03-10 11:03:09 +1100

    ci(docs): set correct CONTEXTIVE_STAGE when deploying docs

--- docs/web/deploy-ci.sh
+++ docs/web/deploy-ci.sh
@@ -1,4 +1,8 @@
 BUCKET_NAME=($(aws ssm get-parameter --name "/$CONTEXTIVE_STAGE/docs/bucketName" --query Parameter.Value --output text))
 SHA=($(git rev-parse --short HEAD))
 VERSION=($(jq .version package.json -r))
-aws s3 sync dist s3://$BUCKET_NAME/ide/$VERSION+$SHA
+PATH=$VERSION
+if [[ "$CONTEXTIVE_STAGE" = "test" ]]; then
+    PATH=$VERSION+$SHA
+fi
+aws s3 sync dist s3://$BUCKET_NAME/ide/$PATH
