b230c6febf8cda007be9d33a4a106f39e81d82d1
Chris Simon <chris@devcycles.io>
2025-03-18 13:23:24 +1100

    docs: starting to restructure to support colocated glossaries

--- docs/web/astro.config.mjs
+++ docs/web/astro.config.mjs
@@ -5,49 +5,50 @@ import versionPlugin from './src/versionPlugin';
 
 // https://astro.build/config
 export default defineConfig({
-	base: process.env.BASE_URL,
-	integrations: [
-		starlight({
-			title: 'Contextive - IDE Extensions',
-			description: 'IDE Extensions to help immerse developers in the language of their domains.',
-			editLink: {
-				baseUrl: 'https://github.com/dev-cycles/contextive/edit/main/docs/web/',
-			},
-			tagline: 'Get on the same page.',
-			logo: {
-				alt: 'Contextive',
-				replacesTitle: true,
-				light: './src/assets/logos/logo-primary.png',
-				dark: './src/assets/logos/logo-inverted.png'
-			},
-			social: {
-				github: 'https://github.com/dev-cycles/contextive',
-				blueSky: 'https://bsky.app/profile/contextive.tech',
-				linkedin: 'https://www.linkedin.com/company/contextive-tech',
-			},
-			customCss: [
-				'./src/assets/fonts/Forrest/font-face.css',
-				'@fontsource/atkinson-hyperlegible/400.css',
-				'./src/styles/custom.css',
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Background',
-					autogenerate: { directory: 'background' }
-				},
-				// {
-				// 	label: 'Reference',
-				// 	autogenerate: { directory: 'reference' },
-				// },
-				'changelog',
-			],
-			plugins: [
-				versionPlugin,
-			],
-		}),
-	],
+    base: process.env.BASE_URL,
+    redirects: {
+        "/guides/usage": "/guides/defining-terminology/"
+    },
+    integrations: [starlight({
+        title: 'Contextive - IDE Extensions',
+        description: 'IDE Extensions to help immerse developers in the language of their domains.',
+        editLink: {
+            baseUrl: 'https://github.com/dev-cycles/contextive/edit/main/docs/web/',
+        },
+        tagline: 'Get on the same page.',
+        logo: {
+            alt: 'Contextive',
+            replacesTitle: true,
+            light: './src/assets/logos/logo-primary.png',
+            dark: './src/assets/logos/logo-inverted.png'
+        },
+        social: {
+            github: 'https://github.com/dev-cycles/contextive',
+            blueSky: 'https://bsky.app/profile/contextive.tech',
+            linkedin: 'https://www.linkedin.com/company/contextive-tech',
+        },
+        customCss: [
+            './src/assets/fonts/Forrest/font-face.css',
+            '@fontsource/atkinson-hyperlegible/400.css',
+            './src/styles/custom.css',
+        ],
+        sidebar: [
+            {
+                label: 'Guides',
+                autogenerate: { directory: 'guides' },
+            },
+            {
+                label: 'Background',
+                autogenerate: { directory: 'background' }
+            },
+            // {
+            // 	label: 'Reference',
+            // 	autogenerate: { directory: 'reference' },
+            // },
+            'changelog',
+        ],
+        plugins: [
+            versionPlugin,
+        ],
+		})],
 });
